77f9c7ce9ca67d793a653a0df6ff990f13514266
Yuto Ogasawara <yutoogasawara@icloud.com>
2026-06-10 11:33:19 -0700

    added specific messages for some commands and small visual fixes

--- main.lua
+++ main.lua
@@ -36,6 +36,26 @@ local input
 local lastImage = 0
 local lastSound = 0
 
+local function EditFreeze(soundInd, asciiInd, editText)
+    local loadingAscii = io.open("imageAscii/" .. tostring(asciiInd))
+    if not loadingAscii then return end
+    local ascii = loadingAscii:read("a")
+    loadingAscii:close()
+    Play(soundInd)
+    local _, count = string.gsub(ascii, "\n", "")
+    local firstNewline = utf8.len(ascii) / count
+    local centeringSpaces = string.rep(" ", Round((firstNewline / 2) - ((string.len(editText)) / 2)) - 1)
+    for i = 0, 50 do
+        ClearCons()
+        io.write(centeringSpaces .. editText)
+        io.write(string.rep("\n", 6 + Round((math.sin(i)) * math.min((i - 45) * 0.11, 0))))
+        --6+(\sin(x))\cdot\min\left((x-30)\cdot0.12,0\right) for desmos graph (absolute goat)
+        print(ascii)
+        Sleep(SleepTimes[soundInd] / 50)
+    end
+    ClearCons()
+end
+
 while true do
     if isLinuxy then
         local location = io.popen("pwd", "r")
@@ -58,28 +78,22 @@ while true do
         asciiInd = math.random(1, 11)
     end
     lastImage = asciiInd
-    local loadingAscii = io.open("imageAscii/" .. tostring(asciiInd))
-    if not loadingAscii then return end
-    local ascii = loadingAscii:read("a")
-    loadingAscii:close()
-    local _, count = string.gsub(ascii, "\n", "")
     local soundInd = math.random(1, 12)
     while soundInd == lastSound do
         soundInd = math.random(1, 12)
     end
     lastSound = soundInd
-    Play(soundInd)
-    local firstNewline = utf8.len(ascii) / count
-    local centeringSpaces = string.rep(" ", Round((firstNewline / 2) - ((string.len(input) + 2) / 2)) - 1)
-    for i = 0, 50 do
-        ClearCons()
-        io.write(centeringSpaces .. "\"" .. input .. "\"")
-        io.write(string.rep("\n", 6 + Round((math.sin(i)) * math.min((i - 45) * 0.1, 0))))
-        --6+(\sin(x))\cdot\min\left((x-30)\cdot0.1,0\right) for desmos graph (absolute goat)
-        print(ascii)
-        Sleep(SleepTimes[soundInd] / 50)
+    local message = "\"" .. input .. "\""
+    if string.find(input, "echo", 1, true) == 1 or string.find(input, "Write-Host", 1, true) == 1 then
+        message = "Bro is talking"
+    elseif string.find(input, "pwd", 1, true) == 1 then
+        message = "He need location"
+    elseif string.find(input, "dir", 1, true) == 1 or string.find(input, "ls", 1, true) == 1 then
+        message = "Bro is searching"
+    elseif string.find(input, "cat", 1, true) == 1 or string.find(input, "type", 1, true) == 1 or string.find(input, "Get-Content", 1, true) == 1 then
+        message = "Bro is reading the file"
     end
-    ClearCons()
+    EditFreeze(soundInd, asciiInd, message)
     os.execute(input)
 end
 ::exit::
