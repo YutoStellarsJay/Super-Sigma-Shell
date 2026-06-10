09c6abcd460147fec988d3dc1add8fc968652f09
Yuto Ogasawara <yutoogasawara@icloud.com>
2026-06-10 10:11:23 -0700

    fixed memory leak and some small fixes, I committed using the super sigma shell

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Super Sigma Shell
 This is a Shell that runs over your Shell on Linux or Unix and probably wont work in Windows. This was so I can practice normal lua.
+
+## Inspiration
+The "So Tuff" Minecraft Java mod made by Ekical on Modrinth
--- main.lua
+++ main.lua
@@ -38,11 +38,14 @@ local lastSound = 0
 
 while true do
     if isLinuxy then
-        local read = io.popen("pwd", "r"):read("a")
-        local temp = string.find(string.reverse(read), "/")
+        local location = io.popen("pwd", "r")
+        if not location then return end
+        local out = location:read("a")
+        location:close()
+        local temp = string.find(string.reverse(out), "/")
         if not temp then return end
         temp = temp - 2
-        io.write("💀:\\🗣️  " .. string.sub(read, #read - temp, #read - 1) .. " % ")
+        io.write("💀:\\🗣️  " .. string.sub(out, #out - temp, #out - 1) .. " % ")
     else
         io.write("🗿@🤫 ")
     end
@@ -50,25 +53,31 @@ while true do
     if input == "exit" then
         goto exit
     end
-    local temp = math.random(1, 8)
-    while temp == lastImage do
-        temp = math.random(1, 8)
+    local asciiInd = math.random(1, 11)
+    while asciiInd == lastImage do
+        asciiInd = math.random(1, 11)
     end
-    lastImage = temp
-    local ascii = io.open("imageAscii/" .. tostring(temp)):read("a")
+    lastImage = asciiInd
+    local loadingAscii = io.open("imageAscii/" .. tostring(asciiInd))
+    if not loadingAscii then return end
+    local ascii = loadingAscii:read("a")
+    loadingAscii:close()
     local _, count = string.gsub(ascii, "\n", "")
-    temp = math.random(1, 11)
-    while temp == lastSound do
-        temp = math.random(1, 11)
+    local soundInd = math.random(1, 12)
+    while soundInd == lastSound do
+        soundInd = math.random(1, 12)
     end
-    lastSound = temp
-    Play(temp)
+    lastSound = soundInd
+    Play(soundInd)
+    local firstNewline = utf8.len(ascii) / count
+    local centeringSpaces = string.rep(" ", Round((firstNewline / 2) - ((string.len(input) + 2) / 2)) - 1)
     for i = 0, 50 do
         ClearCons()
+        io.write(centeringSpaces .. "\"" .. input .. "\"")
         io.write(string.rep("\n", 6 + Round((math.sin(i)) * math.min((i - 45) * 0.1, 0))))
         --6+(\sin(x))\cdot\min\left((x-30)\cdot0.1,0\right) for desmos graph (absolute goat)
         print(ascii)
-        Sleep(SleepTimes[temp] / 50)
+        Sleep(SleepTimes[soundInd] / 50)
     end
     ClearCons()
     os.execute(input)
